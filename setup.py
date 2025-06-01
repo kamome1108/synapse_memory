5c39a5da107d46b5a11f081e6a2a805ef07b2e28
kamome1108 <h.ryuki0817@gmail.com>
2025-06-01 22:01:41 +0900

    feat: Changed license to Business Source License (BUSL) 1.1

--- setup.py
+++ setup.py
@@ -12,17 +12,17 @@ setup(
         'numpy',
         'chromadb',
     ],
-    author='Ryuki Horita', 
+    author='kamome', # 作者名をkamomeに変更
     description='An experience-based memory system for AI, leveraging SQLite and ChromaDB.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/kamome1108/synapse_memory', 
+    url='https://github.com/kamome1108/synapse_memory',
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'License :: OSI Approved :: MIT License',
+        'License :: Other/Proprietary License', # BUSLは通常、OSI承認ではないためこの分類を使用
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
